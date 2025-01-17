9b4abb7e66364fc50e46637516aaff4e828b3dea
alexjesusbm <alexjesusbm@gmail.com>
2025-01-17 12:02:50 -0300

    .

---
+++ app/produto/[id]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
+export default function Produto() {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h1>Nome do gato: {id}</h1>
+    </div>
+  );
+}
